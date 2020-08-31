7183a8cfd882e1a3788770d3f4e6cfd14f8e80bb
jhcode14 <jhsucode@gmail.com>
2020-08-30 23:36:10 -0700

    initial upload

---
+++ README.md
@@ -0,0 +1,28 @@
+This is a test for attempting to creat a project.
+
+by Jason Hsu
+
+
+developer log:
+Day 1:
+- Learned how to run Visual Studio Code
+- Review/Learn Python3
+- Learned how to use Request
+- Learned how to debug Request
+- Goal Achieve: Download Costco's website code into my program's textfile!!
+
+Day 2:
+- Must log in to see Member Only Item Price
+- Redirect to focus on one section first
+- Redirect to focus on non-member only items
+
+Day 3:
+- Started analyzing specific page with lists of proeucts
+- found https://thispointer.com/python-search-strings-in-a-file-and-get-line-numbers-of-lines-containing-the-string/
+- This link demosntrates how to put multiple things in an array of recording text
+- oepned_file.read() gives you single words per speace, not the entire line!
+
+Day 4:
+- solidify excell recording list pattern
+    want name(x), link(x), price + date(y) 
+- need a initial program that checks if excel page exists?
---
+++ analyze.py
@@ -0,0 +1,38 @@
+import xlwt
+from datetime import date
+
+php1_lines = []
+with open ("Monitor List.md", "rt") as myfile:
+    for line in myfile:
+        php1_lines.append(line)
+
+#catagorize things in order
+line_number = 0
+list_of_results = []
+#might need to change to array of array to better format the list
+
+for line in php1_lines:
+    line_number +=1
+    if line.find("<title>") != -1:
+        list_of_results.append((line_number,line.rstrip().strip("<title>").strip("| Costco</title>").strip()))
+for elem in list_of_results:
+    print('line numb = ', elem[0], 'line = ', elem[1])
+
+wb = xlwt.Workbook()
+newsheet = wb.add_sheet(list_of_results[0][1])
+wb.save(str(date.today()))
+
+
+"""
+for line in php1_lines:
+    line_number +=1
+    if line.find("<title>") != -1:
+        list_of_results.append((line_number,line.rstrip().strip("<title>").strip("| Costco</title>").strip()))
+print('ms1')
+for elem in list_of_results:
+    print('line numb = ', elem[0], 'line = ', elem[1])
+    print('ms2')
+print('ms3')
+
+.replace("<title>","")
+"""
